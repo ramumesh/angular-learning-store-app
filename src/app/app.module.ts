951d192c35d2f9ed27498a21a8d8f2f6b1617342
ramumesh <ram.umesh007@gmail.com>
2019-06-05 15:00:01 -0500

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, TopBarComponent, ProductListComponent ],
+  declarations: [ AppComponent, HelloComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
